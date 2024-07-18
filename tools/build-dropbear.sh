2748ab9a000ab69157245bcc961eb5b008becfdc
Sledmine <40512223+Sledmine@users.noreply.github.com>
2024-07-17 21:57:35 -0600

    add curl and dropbear build scripts

---
+++ tools/build-dropbear.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -e
+set -x
+
+BASE=`pwd`
+SRC=$BASE/src
+WGET="wget --prefer-family=IPv4 -nc"
+DEST=$BASE/opt
+LDFLAGS="-L$DEST/lib -Wl,--gc-sections"
+CPPFLAGS="-I$DEST/include"
+CFLAGS="-ffunction-sections -fdata-sections"
+CXXFLAGS=$CFLAGS
+CC="$(which mipsel-linux-musl-gcc)"
+CXX="$(which mipsel-linux-musl-g++)"
+AR="$(which mipsel-linux-musl-ar)"
+RANLIB="$(which mipsel-linux-musl-ranlib)"
+CONFIGURE="./configure --prefix=/opt --host=mipsel-linux"
+MAKE="make -j`nproc`"
+mkdir -p $SRC
+
+######## ####################################################################
+# ZLIB # ####################################################################
+######## ####################################################################
+
+mkdir -p $SRC/zlib && cd $SRC/zlib
+$WGET http://zlib.net/fossils/zlib-1.2.8.tar.gz
+tar zxvf zlib-1.2.8.tar.gz
+cd zlib-1.2.8
+
+CC=$CC \
+CX=$CXX \
+AR=$AR \
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+CXXFLAGS=$CXXFLAGS \
+CROSS_PREFIX=mipsel-linux- \
+./configure \
+--prefix=/opt
+
+$MAKE
+make install DESTDIR=$BASE
+
+######## ####################################################################
+# DROPBEAR # ####################################################################
+######## ####################################################################
+
+mkdir -p $SRC/dropbear && cd $SRC/dropbear
+$WGET https://matt.ucc.asn.au/dropbear/dropbear-2024.85.tar.bz2
+tar jxvf dropbear-2024.85.tar.bz2
+cd dropbear-2024.85
+
+CC=$CC \
+CX=$CXX \
+AR=$AR \
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+CXXFLAGS=$CXXFLAGS \
+CROSS_PREFIX=mipsel-linux- \
+./configure \
+--prefix=/opt --host=mipsel-linux
+
+$MAKE
+make install DESTDIR=$BASE
